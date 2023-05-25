62ca0c43d4d6af2e433481430dd084f57a294c6c
antonsadovskiy <antonsadovskiy6@gmail.com>
2023-05-25 20:11:30 +0300

    first commit

---
+++ src/components/Navigation/Navigation.tsx
@@ -0,0 +1,29 @@
+import React from 'react';
+import { NavLink } from 'react-router-dom';
+import style from './Navigation.module.css'
+
+const Navigation = () => {
+    return (
+        <div>
+            <NavLink className={({isActive}) => isActive ? style.active : ""} to={'/login'}>
+                login
+            </NavLink>
+            <NavLink className={({isActive}) => isActive ? style.active : ""} to={'/registration'}>
+                registration
+            </NavLink>
+            <NavLink className={({isActive}) => isActive ? style.active : ""} to={'/recoveryPassword'}>
+                forgot password?
+            </NavLink>
+            <NavLink className={({isActive}) => isActive ? style.active : ""} to={'/newPassword'}>
+                new password
+            </NavLink>
+            <NavLink className={({isActive}) => isActive ? style.active : ""} to={'/profile'}>
+                profile</NavLink>
+            <NavLink className={({isActive}) => isActive ? style.active : ""} to={'/superComponents'}>
+                super components
+            </NavLink>
+        </div>
+    );
+};
+
+export default Navigation;
